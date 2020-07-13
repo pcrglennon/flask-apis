8723de59e6fec81c0579210554555b258acc6aad
Peter Glennon <pcrglennon@gmail.com>
2020-07-13 13:05:32 -0400

    Fetch actor names and include in response

--- flask_apis/movie_db_utils/app.py
+++ flask_apis/movie_db_utils/app.py
@@ -15,53 +15,53 @@ def actors_crossover():
     actor_ids = [int(id_string) for id_string in actor_id_strings]
 
     loop = asyncio.get_event_loop()
-    all_actor_credits = loop.run_until_complete(fetch_all_actor_credits(actor_ids))
+    actors_data = loop.run_until_complete(fetch_actors_data(actor_ids))
 
-    movies_id_map = build_movies_id_map(all_actor_credits)
+    movies_id_map = build_movies_id_map(actors_data)
 
     crossover_movies = list(filter(lambda movie: len(movie['credits']) > 1, movies_id_map.values()))
 
     return {
-        'actor_ids': actor_ids,
-        'actor_credits': all_actor_credits,
         'crossover_movies': crossover_movies
     }
 
-async def fetch_all_actor_credits(actor_ids: [int]):
+async def fetch_actors_data(actor_ids: [int]):
     async with httpx.AsyncClient() as client:
-        actor_credits = await asyncio.gather(*[fetch_actor_credits(client, actor_id) for actor_id in actor_ids])
+        actors_data = await asyncio.gather(*[fetch_actor_data(client, actor_id) for actor_id in actor_ids])
 
-        return actor_credits
+        return actors_data
 
-async def fetch_actor_credits(client: httpx.AsyncClient, actor_id: int):
+async def fetch_actor_data(client: httpx.AsyncClient, actor_id: int):
     tmdb_api_key = os.getenv('TMDB_API_KEY')
 
     response = await client.get(
-        f'https://api.themoviedb.org/3/person/{actor_id}/movie_credits',
+        f'https://api.themoviedb.org/3/person/{actor_id}?append_to_response=movie_credits',
         params={ 'api_key': tmdb_api_key, 'language': 'en-US' }
     )
 
-    cast_credits = response.json().get('cast')
+    response_body = response.json()
+    actor_name = response_body.get('name')
+    cast_credits = response_body.get('movie_credits', {}).get('cast')
 
-    return { 'actor_id': actor_id, 'credits': cast_credits }
+    return { 'id': actor_id, 'name': actor_name, 'cast_credits': cast_credits }
 
-def build_movies_id_map(all_actor_credits: [dict]):
+def build_movies_id_map(actors_data: [dict]):
     movies_id_map = {}
 
-    for credits_object in all_actor_credits:
-        actor_id = credits_object['actor_id']
-        actor_credits = credits_object['credits']
+    for actor_object in actors_data:
+        actor_id, actor_name, actor_credits = map(actor_object.get, ('id', 'name', 'cast_credits'))
 
-        for actor_credit in actor_credits:
-            movie_id = actor_credit['id']
+        for credit_object in actor_credits:
+            movie_id = credit_object['id']
             movie = movies_id_map.get(movie_id)
-            character, credit_id = map(actor_credit.get, ('character', 'credit_id'))
+            character, credit_id = map(credit_object.get, ('character', 'credit_id'))
 
             if not movie:
-                movie = build_movie_object(movie_id, actor_credit)
+                movie = build_movie_object(movie_id, credit_object)
 
             movie['credits'].append({
                 'actor_id': actor_id,
+                'actor_name': actor_name,
                 'character': character,
                 'credit_id': credit_id
             })
@@ -69,15 +69,17 @@ def build_movies_id_map(all_actor_credits: [dict]):
 
     return movies_id_map
 
-def build_movie_object(movie_id: int, actor_credit: dict):
+def build_movie_object(movie_id: int, credit_object: dict):
     return {
         'id': movie_id,
-        'title': actor_credit['title'],
-        'release_date': actor_credit['release_date'],
-        'poster_path': actor_credit['poster_path'],
+        'title': credit_object['title'],
+        'release_date': credit_object['release_date'],
+        'poster_path': credit_object['poster_path'],
         'credits': []
     }
 
+# Example response:
+#
 # [
 #     {
 #         'id': 11,
@@ -87,11 +89,13 @@ def build_movie_object(movie_id: int, actor_credit: dict):
 #         'credits': [
 #             {
 #                 'actor_id': 2,
+#                 'actor_name': 'Mark Hamill',
 #                 'character': 'Luke Skywalker',
 #                 'credit_id': '52fe420dc3a36847f8000441'
 #             },
 #             {
 #                 'actor_id': 3,
+#                 'actor_name': 'Harrison Ford',
 #                 'character': 'Han Solo',
 #                 'credit_id': '52fe420dc3a36847f8000445'
 #             }
